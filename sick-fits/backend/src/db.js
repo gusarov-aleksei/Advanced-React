4d48d4a871f666d2feb0214a6af072fc7b9ca014
Aleksei Gusarov <alexgusarov@Alexs-MacBook-Pro.local>
2019-01-12 12:18:45 +0400

    Some server side graphql

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,11 @@
+//connection to remote Prisma DB. It gives possibility to query from JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+});
+
+module.exports = db;
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutations = {
 
-module.exports = mutations;
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+            data: {...args}
+        }, info);
+        return item;
+    }
+};
+
+        /*global.dogs = global.dogs || [];
+        //create a dog
+        const newDog = {name: args.name};
+        global.dogs.push(newDog);
+        return newDog;*/
+
+module.exports = Mutations;
